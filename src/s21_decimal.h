f123ac53592ebe4c4be030c4c25d10a33a4bc469
mushroot <mushroot@student.21-school.ru>
2025-08-24 19:27:43 +0300

    s21_decimal.h functions

---
+++ src/s21_decimal.h
@@ -0,0 +1,22 @@
+#ifndef S21_DECIMAL_H
+#define S21_DECIMAL_H
+
+#define LONG_BITS 192
+
+typedef struct
+{
+    int bits[4];
+} s21_decimal;
+
+typedef struct
+{
+    int bits[LONG_BITS];
+    char floa;
+    char sign;
+} s21_long_decimal;
+
+int s21_add(s21_decimal value1, s21_decimal value2, s21_decimal *result);
+int s21_sub(s21_decimal value1, s21_decimal value2, s21_decimal *result);
+int s21_mul(s21_decimal value1, s21_decimal value2, s21_decimal *result);
+
+#endif
