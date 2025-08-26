e8e0c86fa8660c3984ec4094126687cb68778981
Saiyan Quaithe <saiyanqu@student.21-school.ru>
2025-08-26 15:07:54 +0300

    exp -> exponent

--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -2,48 +2,6 @@
 #include <math.h>
 
 
-#define SIGNBIT 0x80000000 
-
-void s21_print_decimal(s21_decimal dec) {
-    printf("Decimal: [");
-    for (int i = 0; i < 4; i++) {
-        printf("0x%08X", dec.bits[i]);
-        if (i < 3) printf(", ");
-    }
-    printf("]\n");
-}
-
-// int main() {
-//     // s21_decimal result;
-//     // int boom = 6789;
-//     // int status = s21_from_int_to_decimal(boom, &result);
-
-//     // printf("%d\n", result.bits[0]);
-//     // printf("%d\n", result.bits[3]);
-//     // print_decimal(result);
-
-
-//     s21_decimal decimal = {0}; 
-//     float res = 0.0;
-//     // decimal.bits[0] = 1234;
-//     // decimal.bits[1] = 0;  
-//     // decimal.bits[2] = 0;
-//     // decimal.bits[3] = 0; 
-    
-//     int stat;
-//     float num = 1234.56;
-//     stat = s21_from_float_to_decimal(num, &decimal);
-//     printf("Test 1: %f -> status: %d\n", num, stat);
-//     s21_print_decimal(decimal); 
-
-
-//     s21_from_decimal_to_float(decimal, &res);
-//     printf("%f\n", res);
-//     s21_print_decimal(decimal);
-
-//     return 0;
-// };
-
 int s21_get_sign(s21_decimal src) {
     return (src.bits[3] & SIGNBIT) ? 1 : 0;
 }
@@ -70,14 +28,14 @@ int s21_negate(s21_decimal value, s21_decimal *result) {
     return status;
 }
 
-void s21_zero_to_hero(s21_decimal *dst) {
+void s21_init(s21_decimal *dst) {
     dst->bits[0] = dst->bits[1] = dst->bits[2] = dst->bits[3] = 0;
 }
 
 int s21_from_int_to_decimal(int src, s21_decimal *dst) {
     int status = 1;
     if (dst != NULL) {
-        s21_zero_to_hero(dst);  
+        s21_init(dst);  
         if (src < 0) {
             dst->bits[3] = SIGNBIT;       
             dst->bits[0] = (unsigned int)(-src);
@@ -101,7 +59,7 @@ int s21_from_decimal_to_int(s21_decimal src, int *dst) {
                 int_part += (1ULL << i);
             }
         }
-        unsigned int scale = s21_get_exp(src);
+        unsigned int scale = s21_get_exponent(src);
         for (unsigned int i = 0; i < scale; i++) {
             int_part /= 10;
         }
@@ -141,10 +99,10 @@ int s21_from_decimal_to_float(s21_decimal src, float *dst) {
             }
             factor *= 2.0;
         }
-        unsigned scale = (src.bits[3] & 0x00FF0000) >> 16;
-        int exp = (int)scale;
+        unsigned int scale = (src.bits[3] & 0x00FF0000) >> 16;
+        int exponent = (int)scale;
     
-        for (int i = 0; i < exp; i++) {
+        for (int i = 0; i < exponent; i++) {
             res /= 10.0;
         }
 
@@ -158,7 +116,7 @@ int s21_from_decimal_to_float(s21_decimal src, float *dst) {
 }
 
 int s21_from_float_to_decimal(float src, s21_decimal *dst) {
-    s21_zero_to_hero(dst);
+    s21_init(dst);
     int status = 0;
 
     if ((src == 1.0 / 0.0) || (src == -1.0 / 0.0) || (src != src) || (!dst)) {
@@ -175,13 +133,13 @@ int s21_from_float_to_decimal(float src, s21_decimal *dst) {
         }
         
         unsigned int float_bits = *(unsigned int *)&src;
-        int exp = ((float_bits >> 23) & 0xFF) - 127;
-        if (exp >= -1 && exp < 96) {
-            dst->bits[exp / 32] |= (1U << (exp % 32));
+        int exponent = ((float_bits >> 23) & 0xFF) - 127;
+        if (exponent >= -1 && exponent < 96) {
+            dst->bits[exponent / 32] |= (1U << (exponent % 32));
         }
 
         unsigned int mask = 0x400000;
-        for (int i = exp; i > 0 && mask; i--) {
+        for (int i = exponent; i > 0 && mask; i--) {
             if (i <= 96) { 
                 int pos = i - 1;
                 int block= pos / 32;
@@ -203,14 +161,13 @@ int s21_from_float_to_decimal(float src, s21_decimal *dst) {
     return status;
   }
 
-
-unsigned int s21_get_exp(s21_decimal src)
+unsigned int s21_get_exponent(s21_decimal src)
 {
     src.bits[3] &= ~SIGNBIT; 
     return src.bits[3] >> 16; 
 }
 
-void s21_set_exp(s21_decimal *src, unsigned int exp) {
-    if (!src || exp > 28) return;
-    src->bits[3] = (exp << 16) | (src->bits[3] & SIGNBIT);
+void s21_set_exponent(s21_decimal *src, unsigned int exponent) {
+    if (!src || exponent > 28) return;
+    src->bits[3] = (exponent << 16) | (src->bits[3] & SIGNBIT);
 }
--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define SIGNBIT 0x80000000 
+
 typedef struct {
     unsigned int bits[4]; 
 } s21_decimal;
@@ -8,11 +10,11 @@ typedef struct {
 int s21_get_sign(s21_decimal src);
 void s21_set_sign(s21_decimal *src, int sign);
 int s21_negate(s21_decimal value, s21_decimal *result);
-void s21_zero_to_hero(s21_decimal *dst);
+void s21_init(s21_decimal *dst);
 int s21_from_int_to_decimal(int src, s21_decimal *dst);
 int s21_from_float_to_decimal(float src, s21_decimal *dst);
-unsigned int s21_get_exp(s21_decimal src);
-void s21_set_exp(s21_decimal *src, unsigned int exp);
+unsigned int s21_get_exponent(s21_decimal src);
+void s21_set_exponent(s21_decimal *src, unsigned int exponent);
 int s21_from_decimal_to_float(s21_decimal src, float *dst);
 int s21_from_float_to_decimal(float src, s21_decimal *dst);
 int s21_from_decimal_to_int(s21_decimal src, int *dst);
