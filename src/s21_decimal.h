8c1f50c5efbfff34e8c2ceaaca87a95bba3b03b2
mushroot <mushroot@student.21-school.ru>
2025-08-29 10:57:32 +0300

    final

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -1,29 +1,52 @@
-#include <limits.h>
-#include <stdio.h>
-
 #ifndef S21_DECIMAL_H
 #define S21_DECIMAL_H
 
-#define SIGNBIT 0x80000000
+#define LONG_BITS 192
 
-typedef struct {
-  unsigned int bits[3];
-  unsigned int sign : 1;
-  unsigned int exp : 8;
-  unsigned int empty : 23;
-} s21_another_decimal;
+typedef struct
+{
+    unsigned int bits[4];
+} s21_decimal;
 
-int s21_add(s21_another_decimal value1, s21_another_decimal value2,
-            s21_another_decimal *result);
-int s21_sub(s21_another_decimal value1, s21_another_decimal value2,
-            s21_another_decimal *result);
-s21_another_decimal s21_decimal_init();
-void s21_align_exponents(s21_another_decimal *a, s21_another_decimal *b);
-int s21_multiply_by_10_power(s21_another_decimal *value, int power);
-int s21_div_by_10(s21_another_decimal *value, s21_another_decimal *result);
-int s21_compare_absolute(s21_another_decimal a, s21_another_decimal b);
-int s21_get_exponent(s21_another_decimal src);
-void s21_set_exponent(s21_another_decimal *src, unsigned int exp);
-int s21_shift_left(s21_another_decimal *value, int shift);
+typedef struct
+{
+    int bits[LONG_BITS];
+    char floa;
+    char sign;
+} s21_long_decimal;
 
+int s21_add(s21_decimal value1, s21_decimal value2, s21_decimal *result);
+int s21_sub(s21_decimal value1, s21_decimal value2, s21_decimal *result);
+
+void s21_long_add(s21_long_decimal value1, s21_long_decimal value2,
+    s21_long_decimal *result);
+void s21_long_sub(s21_long_decimal value1, s21_long_decimal value2,
+    s21_long_decimal *result);
+void s21_long_mul(s21_long_decimal value1, s21_long_decimal value2,
+    s21_long_decimal *result);
+s21_decimal s21_decimal_init();
+s21_long_decimal s21_long_init();
+void s21_decimal_to_long_decimal(s21_decimal value, s21_long_decimal *result);
+void s21_long_float_check(s21_long_decimal *value_1, s21_long_decimal *value_2);
+int s21_long_comparison(s21_long_decimal value_1, s21_long_decimal value_2);
+void s21_long_add(s21_long_decimal value_1, s21_long_decimal value_2,
+    s21_long_decimal *result);
+void s21_long_sub(s21_long_decimal value_1, s21_long_decimal value_2,
+    s21_long_decimal *result);
+s21_long_decimal s21_long_ten_init();
+void s21_long_to_decimal(s21_long_decimal value, s21_decimal *result);
+void s21_long_mul(s21_long_decimal value_1, s21_long_decimal value_2,
+    s21_long_decimal *result);
+int s21_long_size_check(s21_long_decimal *value);
+s21_long_decimal s21_long_div(s21_long_decimal value_1,
+                s21_long_decimal value_2,
+                s21_long_decimal *result);
+int s21_long_size(s21_long_decimal value);
+void s21_long_bank_round(s21_long_decimal value, s21_long_decimal mod,
+           s21_long_decimal *result);
+int s21_long_to_int(s21_long_decimal value);
+void s21_long_shift(s21_long_decimal *value);
 #endif
+
+
+
--- src/s21_decimal_arifmetics.c
+++ src/s21_decimal_arifmetics.c
@@ -1,200 +1,280 @@
 #include "s21_decimal.h"
 
-s21_another_decimal s21_decimal_init() {
-  s21_another_decimal result = {0};
+int s21_add(s21_decimal value1, s21_decimal value2, s21_decimal *result) {
+  *result = s21_decimal_init();
+  int error = 0;
+  s21_long_decimal longValue1;
+  s21_long_decimal longValue2;
+  s21_long_decimal longResult = s21_long_init();
+  s21_decimal_to_long_decimal(value1, &longValue1);
+  s21_decimal_to_long_decimal(value2, &longValue2);
+
+  s21_long_float_check(&longValue1, &longValue2);
+
+  if (longValue1.sign ^ longValue2.sign) {
+    int comp = s21_long_comparison(longValue1, longValue2);
+    if (comp == 1) {
+      s21_long_sub(longValue1, longValue2, &longResult);
+      longResult.sign = longValue1.sign;
+    } else if (comp == 2) {
+      s21_long_sub(longValue2, longValue1, &longResult);
+      longResult.sign = longValue2.sign;
+    } else {
+      longResult = s21_long_init();
+    }
+  } else {
+    s21_long_add(longValue1, longValue2, &longResult);
+    longResult.sign = longValue1.sign;
+  }
+  error = s21_long_size_check(&longResult);
+  s21_long_to_decimal(longResult, result);
+  return error;
+}
+
+int s21_sub(s21_decimal value1, s21_decimal value2, s21_decimal *result) {
+  *result = s21_decimal_init();
+  int error = 0;
+  s21_long_decimal longValue1;
+  s21_long_decimal longValue2;
+  s21_long_decimal longResult = s21_long_init();
+  s21_decimal_to_long_decimal(value1, &longValue1);
+  s21_decimal_to_long_decimal(value2, &longValue2);
+
+  s21_long_float_check(&longValue1, &longValue2);
+
+  if (longValue1.sign == longValue2.sign) {
+    int comp = s21_long_comparison(longValue1, longValue2);
+    if (comp == 1) {
+      s21_long_sub(longValue1, longValue2, &longResult);
+      longResult.sign = longValue1.sign;
+    } else if (comp == 2) {
+      s21_long_sub(longValue2, longValue1, &longResult);
+      longResult.sign = longValue2.sign < 1;
+    } else {
+      longResult = s21_long_init();
+    }
+  } else {
+    s21_long_add(longValue1, longValue2, &longResult);
+    longResult.sign = longValue1.sign;
+  }
+  error = s21_long_size_check(&longResult);
+  s21_long_to_decimal(longResult, result);
+  return error;
+}
+
+s21_decimal s21_decimal_init() {
+  s21_decimal result = {0};
   return result;
 }
 
-int s21_add(s21_another_decimal value1, s21_another_decimal value2,
-            s21_another_decimal *result) {
+s21_long_decimal s21_long_init() {
+  s21_long_decimal value;
+  for (int i = 0; i < LONG_BITS; i++) {
+    value.bits[i] = 0;
+  }
+  value.floa = 0;
+  value.sign = 0;
+  return value;
+}
+
+void s21_decimal_to_long_decimal(s21_decimal value, s21_long_decimal *result) {
+  *result = s21_long_init();
+  char *val_byte = (char *)value.bits;
+  result->floa = *(val_byte + 14);
+  result->sign = *(val_byte + 15);
+  int counter = 0;
+  for (int i = 0; i < 12; i++) {
+    for (int j = 0; j < 8; j++) {
+      if (*(val_byte + i) & (1 << j))
+        result->bits[counter] = 1;
+      counter++;
+    }
+  }
+}
+
+void s21_long_float_check(s21_long_decimal *value_1,
+                          s21_long_decimal *value_2) {
+  s21_long_decimal value_ten = s21_long_ten_init();
+  value_ten.floa = 1;
+  int sign1 = value_1->sign;
+  int sign2 = value_2->sign;
+  while (value_1->floa != value_2->floa) {
+    if (value_1->floa > value_2->floa)
+      s21_long_mul(*value_2, value_ten, value_2);
+    else
+      s21_long_mul(*value_1, value_ten, value_1);
+  }
+  value_1->sign = sign1;
+  value_2->sign = sign2;
+}
+
+int s21_long_comparison(s21_long_decimal value_1, s21_long_decimal value_2) {
+  int result = 0;
+  for (int i = LONG_BITS - 1; i >= 0 && !result; i--) {
+    if (value_1.bits[i] ^ value_2.bits[i])
+      result = value_1.bits[i] * 1 + value_2.bits[i] * 2;
+  }
+  return result;
+}
+
+void s21_long_add(s21_long_decimal value_1, s21_long_decimal value_2,
+                  s21_long_decimal *result) {
+  *result = s21_long_init();
+  int free_bit = 0;
+  for (int i = 0; i < 192; i++) {
+    result->bits[i] = value_1.bits[i] ^ value_2.bits[i] ^ free_bit;
+    free_bit = (value_1.bits[i] + value_2.bits[i] + free_bit) > 1;
+  }
+  result->floa = value_1.floa;
+}
+
+void s21_long_sub(s21_long_decimal value_1, s21_long_decimal value_2,
+                  s21_long_decimal *result) {
+  *result = s21_long_init();
+  int debt_bit = 0;
+  for (int i = 0; i < 192; i++) {
+    result->bits[i] = value_1.bits[i] ^ value_2.bits[i] ^ debt_bit;
+    if (!value_1.bits[i] && (value_2.bits[i] || debt_bit))
+      debt_bit = 1;
+    else if (value_1.bits[i] && value_2.bits[i] && debt_bit)
+      debt_bit = 1;
+    else
+      debt_bit = 0;
+  }
+  result->floa = value_1.floa;
+}
+
+int s21_long_size_check(s21_long_decimal *value) {
+  int error = 0;
+  int val_sign = value->sign;
+  s21_long_decimal value_ten = s21_long_ten_init();
+  s21_long_decimal value_mod = s21_long_init();
+  value_ten.floa = 1;
+
+  while (s21_long_comparison(value_ten, *value) == 2 && value->floa > 28)
+    value_mod = s21_long_div(*value, value_ten, value);
+
+  while (s21_long_size(*value) > 96 && value->floa)
+    value_mod = s21_long_div(*value, value_ten, value);
+
+  if (s21_long_size(value_mod))
+    s21_long_bank_round(*value, value_mod, value);
+
+  if (!s21_long_size(*value))
+    *value = s21_long_init();
+
+  if (s21_long_size(*value) > 96)
+    error = val_sign * 2 + !val_sign * 1;
+  else if (value->floa > 28)
+    error = 2;
+
+  value->sign = val_sign;
+  return error;
+}
+
+s21_long_decimal s21_long_ten_init() {
+  s21_long_decimal value_ten = s21_long_init();
+  value_ten.bits[1] = 1;
+  value_ten.bits[3] = 1;
+  return value_ten;
+}
+
+void s21_long_to_decimal(s21_long_decimal value, s21_decimal *result) {
   *result = s21_decimal_init();
-  int error = 0;
-  if (value1.sign ^ value2.sign) {
-    if (value1.sign) {
-      value1.sign = 0;
-      error = s21_sub(value2, value1, result);
-    } else {
-      value2.sign = 0;
-      error = s21_sub(value1, value2, result);
+  char *res_byte = (char *)result->bits;
+  *(res_byte + 14) = value.floa;
+  *(res_byte + 15) = value.sign;
+  int counter = 0;
+  for (int i = 0; i < 12; i++) {
+    for (int j = 0; j < 8; j++)
+      if (value.bits[counter++])
+        *(res_byte + i) += 1 << j;
+  }
+}
+
+void s21_long_mul(s21_long_decimal value_1, s21_long_decimal value_2,
+                  s21_long_decimal *result) {
+  *result = s21_long_init();
+  int counter = 0;
+  for (; counter < 192; counter++) {
+    if (value_2.bits[counter])
+      s21_long_add(*result, value_1, result);
+    s21_long_shift(&value_1);
+  }
+  result->floa = value_1.floa + value_2.floa;
+}
+
+
+int s21_long_size(s21_long_decimal value) {
+  int long_size = 0;
+  for (int i = LONG_BITS - 1; !long_size && i >= 0; i--)
+    if (value.bits[i])
+      long_size = i + 1;
+  return long_size;
+}
+
+void s21_long_bank_round(s21_long_decimal value, s21_long_decimal mod,
+                         s21_long_decimal *result) {
+  int val_mod = s21_long_to_int(mod);
+  if (val_mod > 5 || (val_mod == 5 && value.bits[0])) {
+    s21_long_decimal value_one = s21_long_init();
+    value_one.bits[0] = 1;
+    s21_long_add(value, value_one, result);
+    result->sign = value.sign;
+  } else
+    *result = value;
+}
+
+int s21_long_to_int(s21_long_decimal value) {
+  int num = 0;
+  int counter = 0;
+  for (int i = 0; i < 4; i++) {
+    if (!i)
+      counter = 1;
+    else
+      counter += counter;
+    num += value.bits[i] * counter;
+  }
+  return num;
+}
+
+void s21_long_shift(s21_long_decimal *value) {
+  for (int i = LONG_BITS - 1; i > 0; i--)
+    value->bits[i] = value->bits[i - 1];
+  value->bits[0] = 0;
+}
+
+s21_long_decimal s21_long_div(s21_long_decimal value_1,
+                              s21_long_decimal value_2,
+                              s21_long_decimal *result) {
+  *result = s21_long_init();
+  s21_long_decimal value_ten = s21_long_ten_init();
+  s21_long_decimal temp_divider;
+  s21_long_decimal temp_result;
+  int floa_1 = value_1.floa;
+  int floa_2 = value_2.floa;
+  value_1.floa = 0;
+  value_2.floa = 0;
+  for (; s21_long_comparison(value_1, value_2) == 2; floa_1++)
+    s21_long_mul(value_1, value_ten, &value_1);
+  while (s21_long_comparison(value_1, value_2) < 2) {
+    temp_divider = s21_long_init();
+    int counter = 0;
+    for (; s21_long_comparison(value_1, temp_result) < 2 && counter < 192;
+         counter++) {
+      temp_divider.bits[counter] = 1;
+      s21_long_mul(value_2, temp_divider, &temp_result);
+      temp_divider.bits[counter] = 0;
     }
-    return error;
+    temp_divider.bits[counter - 2] = 1;
+    s21_long_add(*result, temp_divider, result);
+    s21_long_mul(value_2, temp_divider, &temp_result);
+    s21_long_sub(value_1, temp_result, &value_1);
   }
 
-  int exp1 = s21_get_exponent(value1);
-  int exp2 = s21_get_exponent(value2);
-  int max_exp = (exp1 > exp2) ? exp1 : exp2;
-
-  s21_another_decimal aligned1 = value1;
-  s21_another_decimal aligned2 = value2;
-  s21_align_exponents(&aligned1, &aligned2);
-  unsigned long long sum = 0;
-  unsigned long long carry = 0;
-
-  for (int i = 0; i < 3; i++) {
-    sum = (unsigned long long)aligned1.bits[i] +
-          (unsigned long long)aligned2.bits[i] + carry;
-    carry = sum >> 32;
-    result->bits[i] = (unsigned int)(sum & 0xFFFFFFFF);
-  }
-  if (carry > 0) {
-    if (max_exp > 0) {
-      s21_another_decimal temp = *result;
-      s21_set_exponent(&temp, max_exp);
-      error = s21_div_by_10(&temp, result);
-      if (!error) {
-        s21_set_exponent(result, max_exp - 1);
-      }
-    } else {
-      error = 1;
-    }
-  } else {
-    s21_set_exponent(result, max_exp);
-  }
-  result->sign = value1.sign;
-  return error;
-}
-
-int s21_sub(s21_another_decimal value1, s21_another_decimal value2,
-            s21_another_decimal *result) {
-  *result = s21_decimal_init();
-  int error = 0;
-  if (value1.sign ^ value2.sign) {
-    if (value1.sign) {
-      value1.sign = 0;
-      error = s21_add(value1, value2, result);
-      result->sign = 1;
-    } else {
-      value2.sign = 0;
-      error = s21_add(value1, value2, result);
-    }
-    return error;
-  }
-
-  s21_align_exponents(&value1, &value2);
-  int comparison = s21_compare_absolute(value1, value2);
-  if (comparison == 0) {
-    *result = s21_decimal_init();
-    return 0;
-  }
-  s21_another_decimal *larger = {0};
-  s21_another_decimal *smaller = {0};
-  int result_sign = 0;
-
-  if (comparison > 0) {
-    larger = &value1;
-    smaller = &value2;
-    result_sign = value1.sign;
-  } else {
-    larger = &value2;
-    smaller = &value1;
-    result_sign = !value1.sign;
-  }
-  unsigned long long borrow = 0;
-
-  for (int i = 0; i < 3; i++) {
-    unsigned long long larger_val = (unsigned long long)larger->bits[i];
-    unsigned long long smaller_val =
-        (unsigned long long)smaller->bits[i] + borrow;
-
-    if (larger_val < smaller_val) {
-      result->bits[i] =
-          (unsigned int)((UINT_MAX + 1) + larger_val - smaller_val);
-      borrow = 1;
-    } else {
-      result->bits[i] = (unsigned int)(larger_val - smaller_val);
-      borrow = 0;
-    }
-  }
-
-  s21_set_exponent(result, s21_get_exponent(value1));
-  result->sign = result_sign;
-
-  return error;
-}
-
-void s21_align_exponents(s21_another_decimal *a, s21_another_decimal *b) {
-  int exp1 = s21_get_exponent(*a);
-  int exp2 = s21_get_exponent(*b);
-
-  if (exp1 < exp2) {
-    s21_multiply_by_10_power(a, exp2 - exp1);
-    s21_set_exponent(a, exp2);
-  } else if (exp2 < exp1) {
-    s21_multiply_by_10_power(b, exp1 - exp2);
-    s21_set_exponent(b, exp1);
-  }
-}
-
-int s21_multiply_by_10_power(s21_another_decimal *value, int power) {
-  int error = 0;
-
-  if (power < 0) {
-    error = 1;
-  } else if (power == 0) {
-    error = 0;
-  } else {
-    for (int i = 0; i < power && error == 0; i++) {
-      s21_another_decimal temp = *value;
-      s21_another_decimal temp2 = *value;
-      s21_shift_left(&temp, 3);
-      s21_shift_left(&temp2, 1);
-      error = s21_add(temp, temp2, value);
-    }
-  }
-
-  return error;
-}
-
-int s21_div_by_10(s21_another_decimal *value, s21_another_decimal *result) {
-  unsigned long long remainder = 0;
-  for (int i = 2; i >= 0; i--) {
-    unsigned long long current =
-        ((unsigned long long)remainder << 32) | value->bits[i];
-    result->bits[i] = (unsigned int)(current / 10);
-    remainder = current % 10;
-  }
-  return (remainder == 0) ? 0 : 1;
-}
-
-int s21_compare_absolute(s21_another_decimal a, s21_another_decimal b) {
-  s21_align_exponents(&a, &b);
-  for (int i = 2; i >= 0; i--) {
-    if (a.bits[i] > b.bits[i])
-      return 1;
-    if (a.bits[i] < b.bits[i])
-      return -1;
-  }
-  return 0;
-}
-
-int s21_get_exponent(s21_another_decimal src) { return src.exp; }
-
-void s21_set_exponent(s21_another_decimal *src, unsigned int exp) {
-  if (!src || exp > 28)
-    return;
-  src->exp = exp;
-}
-
-int s21_shift_left(s21_another_decimal *value, int shift) {
-  int error = 0;
-  if (!value || shift < 0) {
-    error = 1;
-  } else if (shift == 0) {
-    error = 0;
-  } else {
-    for (int s = 0; s < shift && !error; s++) {
-      unsigned int carry_prev = 0;
-
-      for (int i = 0; i < 3; i++) {
-        unsigned int current = value->bits[i];
-        value->bits[i] = (current << 1) | carry_prev;
-        carry_prev = (current >> 31) & 1;
-      }
-      if (carry_prev != 0) {
-        error = 1;
-      }
-    }
-  }
-
-  return error;
+  value_1.floa = floa_1;
+  result->floa = floa_1 - floa_2;
+  for (; result->floa < 0; result->floa += 1)
+    s21_long_mul(*result, value_ten, result);
+  return value_1;
 }
